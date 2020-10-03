d0e49b201cf32de7790a5ead6031548fbba82862
Rhydon <adirmak1@gmail.com>
2020-10-03 21:08:41 +0300

    system halt when finds a threat

--- main_thread.c
+++ main_thread.c
@@ -4,6 +4,7 @@
 #include <linux/gfp.h>
 #include <linux/slab.h>
 #include <linux/module.h>
+#include <linux/reboot.h>
 #include <linux/delay.h>
 
 #include "const.h"
@@ -42,19 +43,24 @@ int main_validation_logic_thread(void* validators_md5) {
         if (MALWARE_DETECTED == persistency_validator(md5->boot_file_md5)) {
             kfree(md5->boot_file_md5);
             md5->boot_file_md5 = register_for_boot();
-            panic("%s\n", "MALWARE DETECTED tried to manipulate patch guard boot persistency\n");
+            __warn_printk("%s\n", "MALWARE DETECTED tried to manipulate patch guard boot persistency");
+            kernel_halt();
         }
         if (MALWARE_DETECTED == self_protect_validator(md5->module_memory_md5, md5->module_file_md5)) {
-            panic("%s\n","MALWARE DETECTED tried to patch patch guard memory or file\n");
+            __warn_printk("%s\n","MALWARE DETECTED tried to patch patch guard memory or file");
+            kernel_halt();
         }
         if (MALWARE_DETECTED == syscall_table_protect_validator(md5->syscall_table_md5)) {
-            panic("%s\n","MALWARE DETECTED tried to patch syscall table\n");
+            __warn_printk("%s\n","MALWARE DETECTED tried to patch syscall table");
+            kernel_halt();
         }
         if (MALWARE_DETECTED == idt_protect_validator(md5->idt_per_cpu_md5)) {
-            panic("%s\n","MALWARE DETECTED tried to patch gdt\n");
+            __warn_printk("%s\n","MALWARE DETECTED tried to patch idt");
+            kernel_halt();
         }
         if (MALWARE_DETECTED == gdt_protect_validator(md5->gdt_per_cpu_md5)) {
-            panic("%s\n","MALWARE DETECTED tried to patch gdt\n");
+            __warn_printk("%s\n","MALWARE DETECTED tried to patch gdt");
+            kernel_halt();
         }
         register_for_shutdown();
         msleep(SECONDS_IN_MILISEC * 10);
